15f9cc3a83dab8f0c6233da94deb28c7f14eb689
mint130 <110026510+mint130@users.noreply.github.com>
2023-05-23 19:27:34 +0900

    Update main.cpp

--- b911027/main.cpp
+++ b911027/main.cpp
@@ -14,10 +14,7 @@ FILE* in_fp, * out_fp;
 
 
 void doTask();
-void join();
-void logIn();
-void logOut();
-void deleteMembership();
+
 void program_exit();;
 int main()
 {
@@ -40,6 +37,7 @@ void doTask()
 	JoinUI joinUI = JoinUI(in_fp, out_fp,memberCollection );
 	LogInUI loginUI = LogInUI(in_fp, out_fp, memberCollection);
 	LogOutUI logoutUI = LogOutUI(in_fp, out_fp, memberCollection);
+	DeleteMembershipUI deletemembershipUI = DeleteMembershipUI(in_fp, out_fp, memberCollection);
 	while (!is_program_exit)
 	{
 		// 입력파일에서 메뉴 숫자 2개를 읽기
@@ -52,12 +50,13 @@ void doTask()
 			{
 			case 1: //회원가입
 
-				//join();
+				cout << "가입" << endl;
 				joinUI.createNewMember();
 				break;
 			case 2: //회원탈퇴
-				//deleteMembership();
-				//DeleteMembershipUI();
+				
+				cout << "탈퇴" << endl;
+				deletemembershipUI.deleteMember();
 				break;
 			}
 			break;
@@ -66,13 +65,11 @@ void doTask()
 			{
 			case 1: //로그인
 				
-				//logIn();
 				cout << "로그인" << endl;
 				loginUI.fillinIDPW();
 				break;
 			case 2: //로그아웃
 
-				//logOut();
 				cout << "로그아웃" << endl;
 				logoutUI.logOut();
 				break;
@@ -93,45 +90,7 @@ void doTask()
 	return;
 }
 
-/*void logIn()
-{
-	printf("로그인\n");
-	char ID[MAX_STRING], password[MAX_STRING];
-	fscanf(in_fp, "%s %s", ID, password);
 
-	fprintf(out_fp, "2.1. 로그인\n");
-	fprintf(out_fp, "> %s %s\n", ID, password);
-}
-void logOut()
-{
-	printf("로그아웃\n");
-	fprintf(out_fp, "2.2. 로그아웃\n");
-}
-void deleteMembership()
-{
-	printf("회원탈퇴\n");
-	fprintf(out_fp, "1.2. 회원탈퇴\n");
-	//fprintf(out_fp, "> %s %s %s %s %s\n", user_type, name, SSN, ID, password);
-}
-void join()
-{
-	printf("회원가입\n");
-	/* !!!!!!! 중요 !!!!!!!!!
-	* 단순히 파일을 통해 입출력하는 방법을 보이기 위한 코드이므로 이 함수에서 그대로 사용하면 안됨.
-	* control 및 boundary class를 이용해서 해당 기능이 구현되도록 해야 함.
-	*/
-	/*char user_type[MAX_STRING], name[MAX_STRING], SSN[MAX_STRING],
-		address[MAX_STRING], ID[MAX_STRING], password[MAX_STRING];
-	// 입력 형식 : 이름, 주민번호, ID, Password를 파일로부터 읽음
-
-	fscanf(in_fp, "%s %s %s %s %s", user_type, name, SSN, ID, password);
-	// 해당 기능 수행
-
-		// 출력 형식
-	fprintf(out_fp, "1.1. 회원가입\n");
-	fprintf(out_fp, "> %s %s %s %s %s\n", user_type, name, SSN, ID, password);
-	
-}*/
 void program_exit()
 {
 	printf("종료\n");
